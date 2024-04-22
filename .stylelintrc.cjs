4c41f02af43b02ff054873e2964fb6ebc1d6496e
nodeexx <156369338+nodeexx@users.noreply.github.com>
2024-04-22 21:47:51 +0200

    Improve Stylelint config

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -22,6 +22,12 @@ module.exports = {
       },
     ],
     'no-descending-specificity': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
   },
   overrides: [
     {
